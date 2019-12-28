d104e1b3dc050a2c294f1c40d0ea1e5de27b4e48
Christopher Hui <christophergkhui@gmail.com>
2019-12-27 17:28:05 -0800

    New production branch

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from api import app
-import os
-
-if __name__ == "__main__":
-    app.debug = False
-    app.config['DATABASE_NAME'] = 'db.sqlite'
-    host = os.environ.get('IP', '127.0.0.1')
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host=host, port=port)
--- api.py
+++ api.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import flask
 from flask_restful import Api
 from flask_cors import CORS
@@ -14,8 +16,6 @@ app.config['JSON_SORT_KEYS'] = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 api = Api(app)
 
-import prof_api.filters as filters
-
 api.add_resource(filters.Course, '/api/courses/<string:professor>')
 api.add_resource(filters.Subject, '/api/subjects/<string:professor>/<string:subject>')
 api.add_resource(filters.Year, '/api/years/<string:professor>/<string:subject>/<string:course>')
@@ -29,36 +29,55 @@ import prof_api.stats as stats
 api.add_resource(stats.allSessions, '/api/stats/<string:professor>')
 api.add_resource(stats.sessionsByYear, '/api/stats/<string:professor>/<string:year>')
 api.add_resource(stats.sessionsByYearFilterSubject, '/api/stats/<string:professor>/<string:year>/<string:subject>')
-api.add_resource(stats.courseByYearFilterSubjectSessions, '/api/stats/<string:professor>/<string:year>/<string:subject>/<string:course>')
-api.add_resource(stats.sessionByYearFilterSubjectSession, '/api/stats/<string:professor>/<string:year>/<string:subject>/<string:course>/<string:section>')
+api.add_resource(stats.courseByYearFilterSubjectSessions,
+                 '/api/stats/<string:professor>/<string:year>/<string:subject>/<string:course>')
+api.add_resource(stats.sessionByYearFilterSubjectSession,
+                 '/api/stats/<string:professor>/<string:year>/<string:subject>/<string:course>/<string:section>')
 
 api.add_resource(stats.sessionsBySubject, '/api/stats/subject/<string:professor>/<string:subject>')
 api.add_resource(stats.generalStatisticsByYear, '/api/stats/years/<string:professor>')
 
 api.add_resource(stats.generalStatistics, '/api/general-stats/<string:professor>')
 api.add_resource(stats.generalStatisticsVerbose, '/api/general-stats-verbose/<string:professor>')
-api.add_resource(stats.generalStatisticsVerboseByCourse, '/api/general-stats-verbose/<string:professor>/<string:subject>/<string:course>')
+api.add_resource(stats.generalStatisticsVerboseByCourse,
+                 '/api/general-stats-verbose/<string:professor>/<string:subject>/<string:course>')
 api.add_resource(stats.generalStatisticsSubject, '/api/general-stats/<string:professor>/<string:subject>')
-api.add_resource(stats.generalStatisticsSubjectCourse, '/api/general-stats/<string:professor>/<string:subject>/<string:course>')
-api.add_resource(stats.generalStatisticsSubjectCourseYear, '/api/general-stats/<string:professor>/<string:subject>/<string:course>/<string:year>')
-api.add_resource(stats.generalStatisticsSubjectCourseYearSection, '/api/general-stats/<string:professor>/<string:subject>/<string:course>/<string:year>/<string:section>')
+api.add_resource(stats.generalStatisticsSubjectCourse,
+                 '/api/general-stats/<string:professor>/<string:subject>/<string:course>')
+api.add_resource(stats.generalStatisticsSubjectCourseYear,
+                 '/api/general-stats/<string:professor>/<string:subject>/<string:course>/<string:year>')
+api.add_resource(stats.generalStatisticsSubjectCourseYearSection,
+                 '/api/general-stats/<string:professor>/<string:subject>/<string:course>/<string:year>/<string:section>')
+
 
 @app.route('/')
 def home():
     return flask.render_template('home.html')
 
+
 @app.route('/courses')
 def courses():
     return flask.render_template('courses.html')
+
 
 @app.route('/date')
 def date():
     return flask.render_template('date.html')
 
+
 @app.route('/course-comparator')
 def course_comparator():
     return flask.render_template('course-selector.html')
 
+
 @app.route('/about')
 def about():
     return flask.render_template('about.html')
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.config['DATABASE_NAME'] = 'db.sqlite'
+    host = os.environ.get('IP', '127.0.0.1')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host=host, port=port)
